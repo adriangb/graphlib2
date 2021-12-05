752ad3f0b36fcb6b4778e2e57b0335cce3cc5dc1
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2021-12-05 08:16:23 -0600

    bug: fix annotations

--- python/graphlib2/__init__.py
+++ python/graphlib2/__init__.py
@@ -1,11 +1,6 @@
 from __future__ import annotations
 
-from typing import Hashable, TypeVar
-
 from graphlib2.graphlib2 import CycleError
 from graphlib2.graphlib2 import TopologicalSorter as TopologicalSorter
-
-T = TypeVar("T", bound=Hashable)
-
 
 __all__ = ("TopologicalSorter", "CycleError")
--- python/graphlib2/_types.py
+++ python/graphlib2/_types.py
@@ -1,14 +1,14 @@
 from __future__ import annotations
 
 import sys
-from typing import AbstractSet, Tuple, TypeVar
+from typing import AbstractSet, Hashable, Tuple, TypeVar
 
 if sys.version_info < (3, 8):
     from typing_extensions import Protocol
 else:
     from typing import Protocol
 
-KT_co = TypeVar("KT_co", covariant=True)
+KT_co = TypeVar("KT_co", covariant=True, bound=Hashable)
 VT_co = TypeVar("VT_co", covariant=True)
 
 

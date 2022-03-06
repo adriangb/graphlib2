6f09b3edcda009e8ce05b3dbaf368520100c5c3e
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2022-03-06 15:02:15 -0600

    revert 2f494dfa because it breask the graphlib API

--- python/graphlib2/__init__.py
+++ python/graphlib2/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Generic, Hashable, Iterable, List, Optional, Tuple, TypeVar
+from typing import Generic, Hashable, Iterable, Optional, TypeVar
 
 from graphlib2._types import SupportsItems
 from graphlib2.graphlib2 import CycleError
@@ -10,11 +10,10 @@ T = TypeVar("T", bound=Hashable)
 
 
 class TopologicalSorter(Generic[T]):
-    __slots__ = ("_ts", "_static_order")
+    __slots__ = ("_ts",)
 
     def __init__(self, graph: Optional[SupportsItems[T, Iterable[T]]] = None) -> None:
         self._ts: _TopologicalSorter[T] = _TopologicalSorter()
-        self._static_order: Optional[Tuple[T, ...]] = None
         if graph:
             for node, children in graph.items():
                 self._ts.add(node, tuple(children))
@@ -35,23 +34,16 @@ class TopologicalSorter(Generic[T]):
         self._ts.prepare()
 
     def static_order(self) -> Iterable[T]:
-        if self._static_order is not None:
-            return self._static_order
         self.prepare()
-        so: List[T] = []
         while self.is_active():
             node_group = self.get_ready()
-            for node in node_group:
-                so.append(node)
-                yield node
+            yield from node_group
             self._ts.done(tuple(node_group))
-        self._static_order = tuple(so)
 
     def copy(self) -> TopologicalSorter[T]:
-        new = object.__new__(TopologicalSorter)  # type: ignore  # generic is unknown
+        new = object.__new__(TopologicalSorter)
         new._ts = self._ts.copy()
-        new._static_order = self._static_order
-        return new  # type: ignore  # generic is unknown
+        return new
 
 
 __all__ = ("TopologicalSorter", "CycleError")
